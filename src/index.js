dcb8f8053cfffd88bb0f7f44589ef127d7529695
Hsu Waddy <hsuwady2001@gmail.com>
2023-07-22 00:34:54 +0630

    Add-interactive-structure

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
+// main.js
+
 import './style.css';
 import * as todo from './todo';
+import { loadItemsFromLocalStorage, clearCompletedTasks } from './status';
+
 
 const addTaskButton = document.getElementById('add-task-btn');
 const newTaskInput = document.getElementById('new-task-input');
 const clearCompletedButton = document.getElementById('clear-completed-btn');
 
-todo.loadTasksFromLocalStorage();
+loadItemsFromLocalStorage();
 
 addTaskButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  const taskText = newTaskInput.value.trim();
-  if (taskText !== '') {
-    todo.addTodoItem(taskText);
-    newTaskInput.value = '';
-  }
+    event.preventDefault();
+    const taskText = newTaskInput.value.trim();
+    if (taskText !== '') {
+        todo.addTodoItem(taskText);
+        newTaskInput.value = '';
+    }
 });
 
 clearCompletedButton.addEventListener('click', () => {
-  todo.clearCompletedTasks();
-});
+    todo.clearCompletedTasks();
+})
 
 const todoList = document.getElementById('todo-list');
 
 todoList.addEventListener('click', (event) => {
-  const { target } = event;
-  if (target.classList.contains('delete-btn')) {
-    const todoItem = target.parentElement;
-    todo.deleteTask(todoItem);
-  }
+    const { target } = event;
+    if (target.classList.contains('delete-btn')) {
+        const todoItem = target.parentElement;
+        todo.deleteTask(todoItem);
+    }
 });
 
 todo.renderTodoList();
--- src/todo.js
+++ src/todo.js
@@ -1,149 +1,131 @@
-// Array to store the tasks
-let tasks = [];
+// todo.js
 
-// Function to update task indexes
+import { items, updateStatus, saveItemsToLocalStorage } from './status';
+
 function updateIndexes() {
-  tasks.forEach((task, index) => {
-    task.index = index + 1;
-  });
+    items.forEach((task, index) => {
+        task.index = index + 1;
+    });
 }
 
-// Function to save tasks to local storage
-export function saveTasksToLocalStorage() {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-// Function to load tasks from local storage
-export function loadTasksFromLocalStorage() {
-  const savedTasks = localStorage.getItem('tasks');
-  if (savedTasks) {
-    tasks = JSON.parse(savedTasks);
-  }
-}
-
-// Function to delete a task
 export function deleteTask(todoItem) {
-  /* eslint-disable */
-  const taskIndex = tasks.findIndex((task) => task.index === parseInt(todoItem.dataset.index));
-  if (taskIndex !== -1) {
-    tasks.splice(taskIndex, 1);
-    updateIndexes(); // Update task indexes after deleting a task
-    /* eslint-disable */
-    saveTasksToLocalStorage(); // Save updated tasks to local storage
-    renderTodoList(); // Render the updated todo list
-  }
+    const taskIndex = items.findIndex((task) => task.index === parseInt(todoItem.dataset.index));
+    if (taskIndex !== -1) {
+        items.splice(taskIndex, 1);
+        updateIndexes();
+        saveItemsToLocalStorage();
+        renderTodoList();
+    }
 }
-// Function to edit a task
+
 export function editTask(todoItem) {
-  const taskElement = todoItem.querySelector('.task');
-  const taskText = taskElement.textContent;
-  taskElement.innerHTML = '';
-  const editInput = document.createElement('input');
-  editInput.type = 'text';
-  editInput.value = taskText;
-  editInput.classList.add('edit-input');
-  taskElement.appendChild(editInput);
-  editInput.focus();
-  editInput.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-      const newText = editInput.value.trim();
-      if (newText !== '') {
-        taskElement.textContent = newText;
-        todoItem.style.backgroundColor = '';
-        todoItem.querySelector('.more-btn').style.display = 'inline-block';
-        todoItem.querySelector('.delete-btn').style.display = 'none';
-        updateIndexes(); // Update task indexes after editing a task
-        saveTasksToLocalStorage(); // Save updated tasks to local storage
-      }
-    }
-  });
+    const taskElement = todoItem.querySelector('.task');
+    const taskText = taskElement.textContent;
+    taskElement.innerHTML = '';
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.value = taskText;
+    editInput.classList.add('edit-input');
+    taskElement.appendChild(editInput);
+    editInput.focus();
+    editInput.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            const newText = editInput.value.trim();
+            if (newText !== '') {
+                taskElement.textContent = newText;
+                todoItem.style.backgroundColor = '';
+                todoItem.querySelector('.more-btn').style.display = 'inline-block';
+                todoItem.querySelector('.delete-btn').style.display = 'none';
+                updateIndexes();
+                saveItemsToLocalStorage();
+            }
+        }
+    });
 }
 
-// Function to toggle the delete button visibility
 export function toggleDeleteButton(todoItem) {
-  const moreButton = todoItem.querySelector('.more-btn');
-  const deleteButton = todoItem.querySelector('.delete-btn');
-  moreButton.style.display = 'none';
-  deleteButton.style.display = 'inline-block';
+    const moreButton = todoItem.querySelector('.more-btn');
+    const deleteButton = todoItem.querySelector('.delete-btn');
+    moreButton.style.display = 'none';
+    deleteButton.style.display = 'inline-block';
 }
 
-// Function to create a new todo item
 export function createTodoItem(task) {
-  const todoItem = document.createElement('li');
-  todoItem.classList.add('todo-item');
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.classList.add('task-checkbox');
-  checkbox.checked = task.completed;
-  const taskElement = document.createElement('span');
-  taskElement.classList.add('task');
-  taskElement.textContent = task.description;
-  todoItem.setAttribute('data-index', task.index);
-  if (task.completed) {
-    taskElement.classList.add('completed');
-  }
-  const moreButton = document.createElement('button');
-  moreButton.classList.add('more-btn');
-  moreButton.innerHTML = '<i class="material-icons">more_vert</i>';
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('delete-btn');
-  deleteButton.innerHTML = '<i class="material-icons">delete</i>';
-  todoItem.appendChild(checkbox);
-  todoItem.appendChild(taskElement);
-  todoItem.appendChild(moreButton);
-  todoItem.appendChild(deleteButton);
-  moreButton.addEventListener('click', (event) => {
-    event.stopPropagation();
-    toggleDeleteButton(todoItem);
-    editTask(todoItem);
-    todoItem.style.border = 'none';
-  });
-  deleteButton.addEventListener('click', (event) => {
-    event.stopPropagation();
-    deleteTask(todoItem);
-  });
-  checkbox.addEventListener('change', () => {
-    task.completed = checkbox.checked;
+    const todoItem = document.createElement('li');
+    todoItem.classList.add('todo-item');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('task-checkbox');
+    checkbox.checked = task.completed;
+    const taskElement = document.createElement('span');
+    taskElement.classList.add('task');
+    taskElement.textContent = task.description;
+    todoItem.setAttribute('data-index', task.index);
     if (task.completed) {
-      taskElement.classList.add('completed');
-    } else {
-      taskElement.classList.remove('completed');
+        taskElement.classList.add('completed');
     }
-    saveTasksToLocalStorage(); // Save updated tasks to local storage when checkbox is toggled
-  });
-  return todoItem;
+    const moreButton = document.createElement('button');
+    moreButton.classList.add('more-btn');
+    moreButton.innerHTML = '<i class="material-icons">more_vert</i>';
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-btn');
+    deleteButton.innerHTML = '<i class="material-icons">delete</i>';
+    todoItem.appendChild(checkbox);
+    todoItem.appendChild(taskElement);
+    todoItem.appendChild(moreButton);
+    todoItem.appendChild(deleteButton);
+    moreButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        toggleDeleteButton(todoItem);
+        editTask(todoItem);
+        todoItem.style.border = 'none';
+    });
+    deleteButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        deleteTask(todoItem);
+    });
+    checkbox.addEventListener('change', () => {
+        const index = parseInt(todoItem.getAttribute('data-index'));
+        updateStatus(index, checkbox.checked);
+        if (checkbox.checked) {
+            taskElement.classList.add('completed');
+        } else {
+            taskElement.classList.remove('completed');
+        }
+        saveItemsToLocalStorage();
+    });
+    return todoItem;
 }
 
-// Function to render the todo list
 export function renderTodoList() {
-  const todoList = document.getElementById('todo-list');
-  todoList.innerHTML = '';
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = '';
 
-  // Sort tasks by index value before rendering
-  tasks.sort((a, b) => a.index - b.index);
+    items.sort((a, b) => a.index - b.index);
 
-  tasks.forEach((task) => {
-    const todoItem = createTodoItem(task);
-    todoList.appendChild(todoItem);
-  });
+    items.forEach((task) => {
+        const todoItem = createTodoItem(task);
+        todoList.appendChild(todoItem);
+    });
 }
 
-// Function to add a new task to the todo list
 export function addTodoItem(taskText) {
-  const newTask = {
-    description: taskText,
-    completed: false,
-    index: tasks.length + 1,
-  };
-  tasks.push(newTask);
-  renderTodoList();
-  saveTasksToLocalStorage(); // Save updated tasks to local storage when a new task is added
+    const newTask = {
+        description: taskText,
+        completed: false,
+        index: items.length + 1,
+    };
+    items.push(newTask);
+    renderTodoList();
+    saveItemsToLocalStorage();
 }
-
-// Function to clear all completed tasks
 export function clearCompletedTasks() {
-  tasks = tasks.filter((task) => !task.completed);
-  updateIndexes(); // Update task indexes after clearing completed tasks
-  renderTodoList();
-  saveTasksToLocalStorage(); // Save updated tasks to local storage after clearing completed tasks
+    const completedTasks = items.filter((task) => !task.completed);
+    items.length = 0; // Clear the array in-place
+    completedTasks.forEach((task) => {
+        items.push(task);
+    });
+    updateIndexes();
+    renderTodoList();
+    saveItemsToLocalStorage();
 }
