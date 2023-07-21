9cc120f18fa99ea01b27d0dec2d4e90c23b2715c
Hsu Waddy <hsuwady2001@gmail.com>
2023-07-22 00:42:50 +0630

    Update-edit-storage

--- src/status.js
+++ src/status.js
@@ -3,33 +3,33 @@
 export let items = [];
 
 export function updateStatus(index, completed) {
-    const task = items.find((item) => item.index === index);
-    if (task) {
-        task.completed = completed;
-    }
+  const task = items.find((item) => item.index === index);
+  if (task) {
+    task.completed = completed;
+  }
 }
 
 export function saveItemsToLocalStorage() {
-    localStorage.setItem('tasks', JSON.stringify(items));
+  localStorage.setItem('tasks', JSON.stringify(items));
 }
 
 export function loadItemsFromLocalStorage() {
-    const savedItems = localStorage.getItem('tasks');
-    if (savedItems) {
-        items = JSON.parse(savedItems);
-    }
+  const savedItems = localStorage.getItem('tasks');
+  if (savedItems) {
+    items = JSON.parse(savedItems);
+  }
 }
 
 function filterCompletedTasks() {
-    items = items.filter((task) => !task.completed);
+  items = items.filter((task) => !task.completed);
 }
 
 export function clearCompletedTasks() {
-    const completedTasks = items.filter((task) => !task.completed);
-    items.length = 0; // Clear the array in-place
-    completedTasks.forEach((task) => {
-        items.push(task);
-    });
-    updateIndexes();
-    saveItemsToLocalStorage();
+  const completedTasks = items.filter((task) => !task.completed);
+  items.length = 0; // Clear the array in-place
+  completedTasks.forEach((task) => {
+    items.push(task);
+  });
+  updateIndexes();
+  saveItemsToLocalStorage();
 }
--- src/todo.js
+++ src/todo.js
@@ -1,131 +1,134 @@
-// todo.js
-
 import { items, updateStatus, saveItemsToLocalStorage } from './status';
 
 function updateIndexes() {
-    items.forEach((task, index) => {
-        task.index = index + 1;
-    });
+  items.forEach((task, index) => {
+    task.index = index + 1;
+  });
 }
 
 export function deleteTask(todoItem) {
-    const taskIndex = items.findIndex((task) => task.index === parseInt(todoItem.dataset.index));
-    if (taskIndex !== -1) {
-        items.splice(taskIndex, 1);
-        updateIndexes();
-        saveItemsToLocalStorage();
-        renderTodoList();
-    }
+  const taskIndex = items.findIndex((task) => task.index === parseInt(todoItem.dataset.index));
+  if (taskIndex !== -1) {
+    items.splice(taskIndex, 1);
+    updateIndexes();
+    saveItemsToLocalStorage();
+    renderTodoList();
+  }
 }
 
 export function editTask(todoItem) {
-    const taskElement = todoItem.querySelector('.task');
-    const taskText = taskElement.textContent;
-    taskElement.innerHTML = '';
-    const editInput = document.createElement('input');
-    editInput.type = 'text';
-    editInput.value = taskText;
-    editInput.classList.add('edit-input');
-    taskElement.appendChild(editInput);
-    editInput.focus();
-    editInput.addEventListener('keyup', (event) => {
-        if (event.key === 'Enter') {
-            const newText = editInput.value.trim();
-            if (newText !== '') {
-                taskElement.textContent = newText;
-                todoItem.style.backgroundColor = '';
-                todoItem.querySelector('.more-btn').style.display = 'inline-block';
-                todoItem.querySelector('.delete-btn').style.display = 'none';
-                updateIndexes();
-                saveItemsToLocalStorage();
-            }
+  const taskElement = todoItem.querySelector('.task');
+  const taskText = taskElement.textContent;
+  taskElement.innerHTML = '';
+  const editInput = document.createElement('input');
+  editInput.type = 'text';
+  editInput.value = taskText;
+  editInput.classList.add('edit-input');
+  taskElement.appendChild(editInput);
+  editInput.focus();
+  editInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      const newText = editInput.value.trim();
+      if (newText !== '') {
+        taskElement.textContent = newText;
+        todoItem.style.backgroundColor = '';
+        todoItem.querySelector('.more-btn').style.display = 'inline-block';
+        todoItem.querySelector('.delete-btn').style.display = 'none';
+
+        const index = parseInt(todoItem.getAttribute('data-index'));
+        const task = items.find((item) => item.index === index);
+        if (task) {
+          task.description = newText;
+          saveItemsToLocalStorage();
         }
-    });
+      }
+    }
+  });
 }
 
 export function toggleDeleteButton(todoItem) {
-    const moreButton = todoItem.querySelector('.more-btn');
-    const deleteButton = todoItem.querySelector('.delete-btn');
-    moreButton.style.display = 'none';
-    deleteButton.style.display = 'inline-block';
+  const moreButton = todoItem.querySelector('.more-btn');
+  const deleteButton = todoItem.querySelector('.delete-btn');
+  moreButton.style.display = 'none';
+  deleteButton.style.display = 'inline-block';
 }
 
 export function createTodoItem(task) {
-    const todoItem = document.createElement('li');
-    todoItem.classList.add('todo-item');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('task-checkbox');
-    checkbox.checked = task.completed;
-    const taskElement = document.createElement('span');
-    taskElement.classList.add('task');
-    taskElement.textContent = task.description;
-    todoItem.setAttribute('data-index', task.index);
-    if (task.completed) {
-        taskElement.classList.add('completed');
+  const todoItem = document.createElement('li');
+  todoItem.classList.add('todo-item');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.classList.add('task-checkbox');
+  checkbox.checked = task.completed;
+  const taskElement = document.createElement('span');
+  taskElement.classList.add('task');
+  taskElement.textContent = task.description;
+  todoItem.setAttribute('data-index', task.index);
+  if (task.completed) {
+    taskElement.classList.add('completed');
+  }
+  const moreButton = document.createElement('button');
+  moreButton.classList.add('more-btn');
+  moreButton.innerHTML = '<i class="material-icons">more_vert</i>';
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete-btn');
+  deleteButton.innerHTML = '<i class="material-icons">delete</i>';
+  todoItem.appendChild(checkbox);
+  todoItem.appendChild(taskElement);
+  todoItem.appendChild(moreButton);
+  todoItem.appendChild(deleteButton);
+  moreButton.addEventListener('click', (event) => {
+    event.stopPropagation();
+    toggleDeleteButton(todoItem);
+    editTask(todoItem);
+    todoItem.style.border = 'none';
+  });
+  deleteButton.addEventListener('click', (event) => {
+    event.stopPropagation();
+    deleteTask(todoItem);
+  });
+  checkbox.addEventListener('change', () => {
+    const index = parseInt(todoItem.getAttribute('data-index'));
+    updateStatus(index, checkbox.checked);
+    if (checkbox.checked) {
+      taskElement.classList.add('completed');
+    } else {
+      taskElement.classList.remove('completed');
     }
-    const moreButton = document.createElement('button');
-    moreButton.classList.add('more-btn');
-    moreButton.innerHTML = '<i class="material-icons">more_vert</i>';
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('delete-btn');
-    deleteButton.innerHTML = '<i class="material-icons">delete</i>';
-    todoItem.appendChild(checkbox);
-    todoItem.appendChild(taskElement);
-    todoItem.appendChild(moreButton);
-    todoItem.appendChild(deleteButton);
-    moreButton.addEventListener('click', (event) => {
-        event.stopPropagation();
-        toggleDeleteButton(todoItem);
-        editTask(todoItem);
-        todoItem.style.border = 'none';
-    });
-    deleteButton.addEventListener('click', (event) => {
-        event.stopPropagation();
-        deleteTask(todoItem);
-    });
-    checkbox.addEventListener('change', () => {
-        const index = parseInt(todoItem.getAttribute('data-index'));
-        updateStatus(index, checkbox.checked);
-        if (checkbox.checked) {
-            taskElement.classList.add('completed');
-        } else {
-            taskElement.classList.remove('completed');
-        }
-        saveItemsToLocalStorage();
-    });
-    return todoItem;
+    saveItemsToLocalStorage();
+  });
+  return todoItem;
 }
 
 export function renderTodoList() {
-    const todoList = document.getElementById('todo-list');
-    todoList.innerHTML = '';
+  const todoList = document.getElementById('todo-list');
+  todoList.innerHTML = '';
 
-    items.sort((a, b) => a.index - b.index);
+  items.sort((a, b) => a.index - b.index);
 
-    items.forEach((task) => {
-        const todoItem = createTodoItem(task);
-        todoList.appendChild(todoItem);
-    });
+  items.forEach((task) => {
+    const todoItem = createTodoItem(task);
+    todoList.appendChild(todoItem);
+  });
 }
 
 export function addTodoItem(taskText) {
-    const newTask = {
-        description: taskText,
-        completed: false,
-        index: items.length + 1,
-    };
-    items.push(newTask);
-    renderTodoList();
-    saveItemsToLocalStorage();
+  const newTask = {
+    description: taskText,
+    completed: false,
+    index: items.length + 1,
+  };
+  items.push(newTask);
+  renderTodoList();
+  saveItemsToLocalStorage();
 }
 export function clearCompletedTasks() {
-    const completedTasks = items.filter((task) => !task.completed);
-    items.length = 0; // Clear the array in-place
-    completedTasks.forEach((task) => {
-        items.push(task);
-    });
-    updateIndexes();
-    renderTodoList();
-    saveItemsToLocalStorage();
+  const completedTasks = items.filter((task) => !task.completed);
+  items.length = 0; // Clear the array in-place
+  completedTasks.forEach((task) => {
+    items.push(task);
+  });
+  updateIndexes();
+  renderTodoList();
+  saveItemsToLocalStorage();
 }
